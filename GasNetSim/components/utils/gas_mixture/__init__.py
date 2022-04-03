7cdf131b3c1a5c0926f604bfbea4074072b5638e
Yifei Lu <yi.lu@fz-juelich.de>
2022-04-04 00:15:09 +0200

    Clean up for publish version 0.1.0

--- GasNetSim/components/utils/gas_mixture/__init__.py
+++ GasNetSim/components/utils/gas_mixture/__init__.py
@@ -6,7 +6,7 @@
 #    Last change on 12/21/21, 4:57 PM
 #    Last change by yifei
 #   *****************************************************************************
-from .GERG2008 import *
+# from .GERG2008 import *
 # from .thermo.thermo import *
 from .gas_mixture import *
 from .heating_value import *
