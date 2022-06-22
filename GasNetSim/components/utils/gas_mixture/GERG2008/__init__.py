001bdc01a0151fcc8bda6c1a80fd338b44c4b6c6
Yifei Lu <yi.lu@fz-juelich.de>
2022-06-22 12:38:29 +0200

    Add GERG2008 gas mixture EOS

---
+++ GasNetSim/components/utils/gas_mixture/GERG2008/__init__.py
@@ -0,0 +1,10 @@
+#   #!/usr/bin/env python
+#   -*- coding: utf-8 -*-
+#   ******************************************************************************
+#     Copyright (c) 2022.
+#     Developed by Yifei Lu
+#     Last change on 3/21/22, 5:44 PM
+#     Last change by yifei
+#    *****************************************************************************
+from .setup import *
+from .gerg2008 import *
