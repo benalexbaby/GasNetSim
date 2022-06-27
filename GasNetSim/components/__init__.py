89e5aa439f0b33a40722e55bdd103e6633f86378
Yifei Lu <yi.lu@fz-juelich.de>
2022-06-27 10:16:34 +0200

    Fix import path

--- GasNetSim/components/__init__.py
+++ GasNetSim/components/__init__.py
@@ -8,8 +8,9 @@
 #   *****************************************************************************
 import sys
 import os
+from pathlib import Path
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "utils/gas_mixture/thermo"))
+sys.path.append(Path("./utils/gas_mixture/thermo"))
 
 from .node import Node
 from .pipeline import Pipeline
