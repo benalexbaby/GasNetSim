8d0e204ceade42d20d6ae5bf281fecbbf1241879
Aparna Justin <a.justin@fz-juelich.de>
2022-06-30 16:52:58 +0200

    German grid input files added

---
+++ examples/German_resistance/German_resistance.py
@@ -0,0 +1,13 @@
+#   #!/usr/bin/env python
+#   -*- coding: utf-8 -*-
+#   ******************************************************************************
+#     Copyright (c) 2022.
+#     Developed by Yifei Lu
+#     Last change on 6/13/22, 4:47 PM
+#     Last change by Yifei
+#    *****************************************************************************
+import GasNetSim as gns
+from pathlib import Path
+
+network = gns.create_network_from_csv(Path('.'))
+network.simulation()
